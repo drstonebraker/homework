603d9bcd0bbab2b88e425170944184fa9b7e882c
drstonebraker <drew.stonebraker@gmail.com>
2017-08-15 23:28:41 -0700

    Redo project

---
+++ w3d3/rails_project/db/migrate/create_houses.rb
@@ -0,0 +1,11 @@
+class CreateHouses < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :houses do |t|
+      t.string :address, null: false
+
+      t.timestamps
+    end
+  end
+
+end
