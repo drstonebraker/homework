813356317b0e00a12db20bcbb188765eb5e3d431
App Academy Student <student@appacademy.io>
2017-08-24 21:02:08 -0700

    Write form_authenticity_token helper

--- w4d5/helpers-layouts-mailer/app/helpers/application_helper.rb
+++ w4d5/helpers-layouts-mailer/app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def auth_token
+    input = <<-INPUT
+    <input
+      type="hidden"
+      name="authenticity_token"
+      value='#{ form_authenticity_token }'
+    />
+    INPUT
+
+    input.html_safe
+  end
 end
