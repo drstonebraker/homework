1cbdf9d93e455c0fc3e0276d022c9ef5e5ff36d8
drstonebraker <drew.stonebraker@gmail.com>
2017-08-27 22:22:52 -0700

    Write User model specs

--- w5d1/music_app/spec/factories/users.rb
+++ w5d1/music_app/spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
+  pw = Faker::Internet.password
   factory :user do
-    
+    email { Faker::Internet.email }
+    password_digest { BCrypt::Password.create(pw) }
+    password pw
   end
 end
--- w5d1/music_app/spec/models/user_spec.rb
+++ w5d1/music_app/spec/models/user_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:password) do
+    'abc123'
+  end
+
+  subject(:user) do
+    create(:user,
+    password: password,
+    password_digest: BCrypt::Password.create(password))
+  end
+
+  describe 'User validations' do
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_length_of(:password).is_at_least(6) }
+
+  end
+
+  describe 'User#is_password?' do
+    it 'should return true with a valid password' do
+      expect(user.is_password?(password)).to be(true)
+    end
+    it 'should return false with an invalid password' do
+      expect(user.is_password?('password')).to be(false)
+    end
+  end
+
+  describe 'User#reset_session_token!' do
+
+    it 'persists to db' do
+      token = user.reset_session_token!
+      expect(User.find(user.id).session_token).to eq(token)
+    end
+
+    it 'resets token' do
+      token = user.reset_session_token!
+      expect(user.session_token).to eq(token)
+      user.reset_session_token!
+      expect(user.session_token).not_to eq(token)
+    end
+
+  end
+
+  describe 'User::find_by_credentials' do
+    it 'returns nil when email does not exist' do
+      result = User.find_by_credentials('z@zzz.com', password)
+      expect(result).to be(nil)
+    end
+    it 'returns nil when password is not correct' do
+      result = User.find_by_credentials(user.email, 'password')
+      expect(result).to be(nil)
+    end
+    it 'returns user when email and password are valid' do
+      result = User.find_by_credentials(user.email, password)
+      expect(result).to eq(user)
+    end
+  end
+
 end
