8954903bbec1d6f49db2a4ecf47a308b3c487218
drstonebraker <drew.stonebraker@gmail.com>
2017-08-27 22:52:24 -0700

    Write UsersController specs

--- w5d1/music_app/spec/controllers/users_controller_spec.rb
+++ w5d1/music_app/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, {}
+      expect(response).to render_template("new")
+    end
+  end
 
+  describe "POST #create" do
+    context "with invalid params" do
+      it "validates the presence of the user's email and password" do
+        post :create, params: {user: {email: '', password: ''}}
+        expect(response).to redirect_to(new_user_url)
+        expect(flash[:errors]).to be_present
+      end
+
+      it "validates that the password is at least 6 characters long" do
+        post :create, params: {user: {email: 'b@abc.com', password: 'abc'}}
+        expect(response).to redirect_to(new_user_url)
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context "with valid params" do
+      it "redirects user to bands index on success" do
+        post :create, params: {user: {email: 'b@abc.com', password: 'abcdef'}}
+        expect(response).to redirect_to(bands_url)
+        expect(flash[:errors]).to_not be_present
+      end
+    end
+  end
 end
