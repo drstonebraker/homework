cd8b9e6cf687e67e4298c10ce7331e1acd89ecfe
drstonebraker <drew.stonebraker@gmail.com>
2017-08-28 22:06:24 -0700

    Add associations

--- w5d2/polymorphism/test/models/toy_test.rb
+++ w5d2/polymorphism/test/models/toy_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :integer          not null, primary key
+#  name         :string           not null
+#  toyable_id   :integer
+#  toyable_type :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class ToyTest < ActiveSupport::TestCase
