ea86b68f058a1b1f496bfd18bcb99934d3283ef7
drstonebraker <drew.stonebraker@gmail.com>
2017-09-05 23:12:34 -0700

    Finish exercise

--- w6d3/first_ajax/js/index.js
+++ w6d3/first_ajax/js/index.js
@@ -1,5 +1,11 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  type: 'GET',
+  success: (data) => console.log(data)
+})
 
 // Add another console log here, outside your AJAX request
+console.log('end of file');
