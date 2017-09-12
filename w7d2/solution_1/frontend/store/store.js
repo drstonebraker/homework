0a77445abe32b926a52aef6ed74b13ce11f6f4d1
drstonebraker <drew.stonebraker@gmail.com>
2017-09-11 23:21:32 -0700

    Finish middleware exercise

--- w7d2/solution_1/frontend/store/store.js
+++ w7d2/solution_1/frontend/store/store.js
@@ -1,12 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 
+const addLoggingToDispatch = store => next => action => {
+  const dispatch = store.dispatch;
+
+  console.log('state: ', store.getState());
+  console.log('action: ', action);
+  let result = next(action);
+  console.log('state: ', store.getState());
+  return result;
+
+};
+
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(RootReducer, preloadedState);
+  const store = createStore(RootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
-}
+};
 
 export default configureStore;
--- w7d2/solution_1/frontend/todo_redux.jsx
+++ w7d2/solution_1/frontend/todo_redux.jsx
@@ -4,6 +4,8 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
